6b436d4e484a0cc30c3d7e117dacb68d936d4499
Anshita-jain-617 <116895741+Anshita-jain-617@users.noreply.github.com>
2022-11-04 16:56:06 +0530

    Add files via upload

--- patterns.py
+++ patterns.py
@@ -47,7 +47,283 @@
 #         j+=1
 #     print()
 #     i+=1
- 
+
+# f).
+# i=5
+# while i>=1:
+#     j=5
+#     while j>=i:
+#         print(i,end=" ")
+#         j-=1
+#     print()
+#     i-=1
+
+# g).
+# i=5
+# while i>=1:
+#     j=5
+#     while j>=i:
+#         print(j,end=" ")
+#         j-=1
+#     print()
+#     i-=1
+
+# h).
+# i=1
+# while i<=5:
+#     k=1
+#     while k<=5-i:
+#         print(" ",end=" ")
+#         k=k+1
+#     j=1
+#     while j<=i:
+#         print(j,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+#  i).
+# i=1
+# while i<=5:
+#     k=1
+#     while k<=5-i:
+#         print(" ",end=" ")
+#         k+=1
+#     j=1
+#     while j<=i:
+#         print(i,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# j).
+# i=5
+# while i>=1:
+#     b=1
+#     while b<=i-1:
+#         print(" ",end=" ")
+#         b+=1
+#     j=6
+#     while j>i:
+#         print(j-i,end=" ")
+#         j-=1
+#     print()
+#     i-=1
+
+# k).
+# i=5
+# while i>=1:
+#     j=6
+#     while j>i:
+#         print(j-i,end=" ")
+#         j=j-1
+#     print()
+#     i-=1
+
+# l).
+# i=1
+# k=1
+# while i<=4:
+#     j=1
+#     while j<=i:
+#         print(k,end=" ")
+#         k+=1
+#         j+=1
+#     print()
+#     i+=1
+
+# m).
+# i=1
+# k=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print(k,end=" ")
+#         k+=1
+#         j+=1
+#     print()
+#     i+=2
+    
+# n).
+# i=0
+# while i<5:
+#     j=0
+#     while j<=i:
+#         print(i*j,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# o).
+# i=0
+# while i<5:
+#     j=0
+#     while j<=i:
+#         print(j*j,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# p).
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print(i*i,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# Q).
+# i=1
+# while i<=9:
+#     j=1
+#     while j<=i:
+#         print(i,end=" ")
+#         j+=2
+#     print()
+#     i+=2
+
+# r).
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# s).
+# i=5
+# while i>=1:
+#     j=1
+#     while j<=i:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i-=1
+
+# t).
+# i=1
+# while i<=5:
+#     k=1
+#     while k<=5-i:
+#         print("",end=" ")
+#         k+=1
+#     j=1
+#     while j<=i:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# u).
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         print(j,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# v).
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         print(i,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# w).
+# i=1
+# while i<=5:
+#     j=65
+#     while j<65+i:
+#         print(chr(j),end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# x).
+# i=1
+# while i<=5:
+#     j=65
+#     while j<65+i:
+#         print(i,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# y).
+# i=1
+# l=1
+# while i<=4:
+#     k=1
+#     while k<=4-i:
+#         print(" ",end=" ")
+#         k+=1
+#     j=1
+#     while j<=l:
+#         print("*",end=" ")
+#         j+=1
+#     print()
+#     i+=1
+#     l+=2
+
+# z).
+# a="PYTHON"
+# i=0
+# while i<=len(a):
+#     j=0
+#     while j<=i:
+#         print(a[j],end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# aa).
+# a="PYTHON"
+# i=0
+# while i<=len(a):
+#     j=0
+#     while j<=i:
+#         print(a[i],end=" ")
+#         j+=1
+#     print()
+#     i+=1
+
+# ab).
+# i=1
+# l=1
+# while i<=5:
+#     k=1
+#     while k<=5-i:
+#         print(" ",end=" ")
+#         k+=1
+#     j=1
+#     while j<=l:
+#         print(i,end=" ")
+#         j+=1
+#     print()
+#     i+=1
+#     l+=2
+
+
+
+
+        
+
+
+
+
+
+
+
+
 
 
 
